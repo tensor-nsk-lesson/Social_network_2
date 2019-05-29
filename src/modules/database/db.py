a9731385f9167472e80d601be786bb6d3d807bd5
NeTolya <sloupockpe4enka@gmail.com>
2019-05-30 02:32:35 +0700

    Перенес post запросы в отельные ф-ии

--- src/modules/database/db.py
+++ src/modules/database/db.py
@@ -1,6 +1,6 @@
 import psycopg2
 from flask import jsonify
-def execute_sql(query):
+def execute_sql(query, line):
 	conn = psycopg2.connect(host='90.189.168.29', dbname='social_2', user='social_2', password='social_2')
 	cur = conn.cursor()
 	cur.execute(query)
@@ -13,3 +13,25 @@ def execute_sql(query):
 		cur.close()
 		conn.close()
 		return jsonify(ans)
+
+
+def check_table(col):
+	if col is not None:
+		return '*'
+
+
+def get_data(column, table): # принимает колонку, столбец; если все колонки - передать пустое поле
+	column = check_table(column)
+	sql = 'SELECT ' + column + ' FROM \"' + table + '\"'
+	return execute_sql(sql, True)
+
+def get_data_where(column, table, des, id):# Колонка, столбец, искомое, передоваемый id в url
+	column = check_table(column)
+	sql = 'SELECT ' + column + ' FROM \"' + table + '\" u WHERE u.' + des + ' = ' + id.__str__()
+	return execute_sql(sql, False)
+
+def get_data_where_and(column, table, des, id, addition, addition_value): # Колонка, столбец, искомое, передоваемый id в url, название строки, значение строки
+	column = check_table(column)
+	sql = 'SELECT ' + column + ' FROM \"' + table + '\" u WHERE u.' + des + ' = ' + id.__str__() + ' AND u.' + addition + ' = ' + addition_value.__str__()
+	print(sql)
+	return execute_sql(sql, False)
--- src/modules/routes/profile.py
+++ src/modules/routes/profile.py
@@ -6,19 +6,19 @@ from flask import request
 #Запросы профиля
 @routes.route('/get/users', methods=["GET"])
 def get_all_users():
-    return db.execute_sql('SELECT * FROM \"User\"')
+    return db.get_data('', 'User')
 
 
 @routes.route('/get/user/<int:id>', methods=["GET"])
 def profile(id):
-    return db.execute_sql('SELECT * FROM \"User\" u WHERE u.id = ' + id.__str__())  # Выборка конкретного пользователя
+    return db.get_data_where('','User', 'id', id)  # Выборка конкретного пользователя
 
 
 @routes.route('/get/user/<int:id>/friends', methods=["GET"])
 def get_all_friends(id):
-    return db.execute_sql('SELECT friend_id FROM  \"Friendlist\" f WHERE f.user_id = ' + id.__str__() + ' AND f.state_id = 2 ')
+    return db.get_data_where_and('friend_id', 'Friendlist', 'user_id', id, 'state_id', 2)
 
-
+'''
 @routes.route('/profile/<int:id>/friends', methods=["POST"])
 def post_all_friends(id):  
 	data = request.json()
@@ -29,33 +29,29 @@ def post_all_friends(id):
 def put_all_friends(id):  
 	data = request.json()
 	return db.execute_sql('UPDATE Friendlist f SET state_id = 2 WHERE f.user_id = ' + id.__str__() + ' AND friend_id = ' + data["user_id"])
-
+'''
 #Вывод ЧС пользователя
+
 @routes.route('/get/user/<int:id>/blacklist', methods=["GET"])
 def get_one_friend(id):
-    return db.execute_sql('SELECT friend_id FROM  \"Friendlist\" f WHERE f.user_id = ' + id.__str__() + ' AND f.state_id = 3 ')
+  return db.get_data_where_and('friend_id', 'Friendlist', 'user_id', id, 'state_id', 3)
 
-
+'''
 @routes.route('/profile/<int:id>/blacklist', methods=["PUT"])
 def put_one_friend(id):
 	data = request.json()
 	return db.execute_sql('UPDATE Friendlist f SET state_id = 3 WHERE f.user_id = ' + id.__str__() + ' AND friend_id = ' + data["user_id"])
+'''
 
+@routes.route('/get/user/<int:id>/dialogs', methods=["GET"])
+def message(id):
+  return db.get_data_where('dialog_id','AllDialogs', 'user_id', id) 
 
-@routes.route('/profile/<int:u_id>/allmessage', methods=["GET"])
-def get_all_message(u_id):
-    return db.execute_sql('SELECT dialog_id FROM \"AllDialogs\" d WHERE d.user_id = ' + u_id.__str__())
-
-
-@routes.route('/get/user/<int:d_id>/dialogs', methods=["GET"])
-def message(d_id):
-	return db.execute_sql('SELECT dialog_id FROM \"AllDialogs\" d WHERE d.user_id = %s' % id)
-
-
+'''
 @routes.route('/get/user/<int:id>/posts', methods=["GET"])  # Выборка постов пользователя
 def get_user_posts(id):
    return db.execute_sql('SELECT * FROM \"UPosts\" u, \"PContent\" p WHERE u.user_id = %s AND u.post_id = p.id' % id)
-
+'''
 ''' Необходимо добавить функцию в db.py
 @app.route('/post/user/<int:id>/post/new', methods=["POST"])  # Создание поста пользователем
 def create_user_post(id):
