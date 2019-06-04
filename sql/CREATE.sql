41e32bdb0222549fc360e90d0e8e0ed89d37c79b
E1eventh <9998606@gmail.com>
2019-06-04 21:15:59 +0700

    Добавлены таблицы альбомов и фото

--- sql/CREATE.sql
+++ sql/CREATE.sql
@@ -5,10 +5,13 @@
 
 DROP TABLE IF EXISTS"Administrators" CASCADE;
 DROP TABLE IF EXISTS"AllDialogs" CASCADE;
-DROP TABLE IF EXISTS"City" CASCADE;
+DROP TABLE IF EXISTS"CAlbom" CASCADE;
+DROP TABLE IF EXISTS"CPhoto" CASCADE;
 DROP TABLE IF EXISTS"CPosts" CASCADE;
+DROP TABLE IF EXISTS"City" CASCADE;
 DROP TABLE IF EXISTS"Communities" CASCADE;
 DROP TABLE IF EXISTS"Country" CASCADE;
+DROP TABLE IF EXISTS"DPhoto" CASCADE;
 DROP TABLE IF EXISTS"Dialog" CASCADE;
 DROP TABLE IF EXISTS"Friendlist" CASCADE;
 DROP TABLE IF EXISTS"Gender" CASCADE;
@@ -16,9 +19,12 @@ DROP TABLE IF EXISTS"Likes" CASCADE;
 DROP TABLE IF EXISTS"MContent" CASCADE;
 DROP TABLE IF EXISTS"Message" CASCADE;
 DROP TABLE IF EXISTS"PContent" CASCADE;
+DROP TABLE IF EXISTS"Photo" CASCADE;
 DROP TABLE IF EXISTS"Post" CASCADE;
 DROP TABLE IF EXISTS"Roles" CASCADE;
 DROP TABLE IF EXISTS"Topics" CASCADE;
+DROP TABLE IF EXISTS"UAlbom" CASCADE;
+DROP TABLE IF EXISTS"UPhoto" CASCADE;
 DROP TABLE IF EXISTS"UPosts" CASCADE;
 DROP TABLE IF EXISTS"USubs" CASCADE;
 DROP TABLE IF EXISTS"User" CASCADE;
@@ -28,14 +34,31 @@ DROP TABLE IF EXISTS"User" CASCADE;
 --
 
 CREATE TABLE IF NOT EXISTS "Administrators" ( 
-	"comm_id" serial not null,
+	"comm_id" int not null,
 	"user_id" int not null,
 	"role_id" int not null
 ) WITHOUT OIDS;
 
 CREATE TABLE IF NOT EXISTS "AllDialogs" ( 
-	"user_id" serial not null,
+	"user_id" int not null,
 	"dialog_id" int not null
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS "CAlbom" (
+	"id" serial not null,
+	"name" text not null,
+	"comm_id" int not null,
+	PRIMARY KEY ("id")
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS "CPhoto" (
+	"albom_id" int not null,
+	"photo_id" int not null
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS "CPosts" ( 
+	"comm_id" int not null,
+	"post_id" int not null
 ) WITHOUT OIDS;
 
 CREATE TABLE IF NOT EXISTS "City" (
@@ -43,11 +66,6 @@ CREATE TABLE IF NOT EXISTS "City" (
 	"name" text not null,
 	"country_id" int not null,
 	PRIMARY KEY ("id")
-) WITHOUT OIDS;
-
-CREATE TABLE IF NOT EXISTS "CPosts" ( 
-	"comm_id" serial not null,
-	"post_id" int not null
 ) WITHOUT OIDS;
 
 CREATE TABLE IF NOT EXISTS "Communities" ( 
@@ -65,19 +83,25 @@ CREATE TABLE IF NOT EXISTS "Country" (
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
 
+CREATE TABLE IF NOT EXISTS "DPhoto" (
+	"dialog_id" int not null,
+	"photo_id" int not null
+) WITHOUT OIDS;
+
 CREATE TABLE IF NOT EXISTS "Dialog" ( 
 	"id" serial not null,
+	"name" text not null,
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
 
 CREATE TABLE IF NOT EXISTS "Friendlist" ( 
-	"user_id" serial not null,
+	"user_id" int not null,
 	"friend_id" int not null,
 	"state_id" smallint not null
 ) WITHOUT OIDS;
 
 CREATE TABLE IF NOT EXISTS "Gender" ( 
-	"id" serial not null,
+	"id" int not null,
 	"name" text not null,
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
@@ -109,6 +133,12 @@ CREATE TABLE IF NOT EXISTS "PContent" (
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
 
+CREATE TABLE IF NOT EXISTS "Photo" (
+	"id" serial not null,
+	"photo" text not null,
+	PRIMARY KEY ("id")
+) WITHOUT OIDS;
+
 CREATE TABLE IF NOT EXISTS "Post" ( 
 	"id" serial not null,
 	"content_id" int not null,
@@ -128,8 +158,20 @@ CREATE TABLE IF NOT EXISTS "Topics" (
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
 
+CREATE TABLE IF NOT EXISTS "UAlbom" (
+	"id" serial not null,
+	"name" text not null,
+	"user_id" int not null,
+	PRIMARY KEY ("id")
+) WITHOUT OIDS;
+
+CREATE TABLE IF NOT EXISTS "UPhoto" (
+	"albom_id" int not null,
+	"photo_id" int not null
+) WITHOUT OIDS;
+
 CREATE TABLE IF NOT EXISTS "UPosts" ( 
-	"user_id" serial not null,
+	"user_id" int not null,
 	"post_id" int not null
 ) WITHOUT OIDS;
 
@@ -150,9 +192,9 @@ CREATE TABLE IF NOT EXISTS "User" (
 	"birthday" date not null,
 	"country_id" int not null,
 	"city_id" int not null,
-	"photo" text not null,
-	"verification" boolean not null,
-	"private" boolean not null,
+	"photo_id" int not null,
+	"verification" boolean not null default false,
+	"private" boolean not null default false,
 	"deleted" boolean not null default false,
 	PRIMARY KEY ("id")
 ) WITHOUT OIDS;
@@ -173,16 +215,29 @@ ALTER TABLE "AllDialogs"
 ALTER TABLE "AllDialogs" 
 	ADD CONSTRAINT "Dialog_fk_dialog_id" FOREIGN KEY ("dialog_id") REFERENCES "Dialog" ("id");
 
-ALTER TABLE "City"
-	ADD CONSTRAINT "Country_fk_county_id" FOREIGN KEY ("country_id") REFERENCES "Country" ("id");
+ALTER TABLE "CAlbom" 
+	ADD CONSTRAINT "Communities_fk_comm_id" FOREIGN KEY ("comm_id") REFERENCES "Communities" ("id");
+
+ALTER TABLE "CPhoto" 
+	ADD CONSTRAINT "CAlbom_fk_albom_id" FOREIGN KEY ("albom_id") REFERENCES "CAlbom" ("id");
+ALTER TABLE "CPhoto" 
+	ADD CONSTRAINT "Photo_fk_photo_id" FOREIGN KEY ("photo_id") REFERENCES "Photo" ("id");
 
 ALTER TABLE "CPosts" 
 	ADD CONSTRAINT "Communities_fk_comm_id" FOREIGN KEY ("comm_id") REFERENCES "Communities" ("id");
 ALTER TABLE "CPosts" 
 	ADD CONSTRAINT "Post_fk_post_id" FOREIGN KEY ("post_id") REFERENCES "Post" ("id");
 
+ALTER TABLE "City"
+	ADD CONSTRAINT "Country_fk_county_id" FOREIGN KEY ("country_id") REFERENCES "Country" ("id");
+
 ALTER TABLE "Communities" 
 	ADD CONSTRAINT "Topics_fk_topic" FOREIGN KEY ("topic_id") REFERENCES "Topics" ("id");
+
+ALTER TABLE "DPhoto" 
+	ADD CONSTRAINT "Dialog_fk_dialog_id" FOREIGN KEY ("dialog_id") REFERENCES "Dialog" ("id");
+ALTER TABLE "DPhoto" 
+	ADD CONSTRAINT "Photo_fk_photo_id" FOREIGN KEY ("photo_id") REFERENCES "Photo" ("id");
 
 ALTER TABLE "Friendlist" 
 	ADD CONSTRAINT "User_fk_user_id" FOREIGN KEY ("user_id") REFERENCES "User" ("id");
@@ -202,6 +257,14 @@ ALTER TABLE "Message"
 ALTER TABLE "Post" 
 	ADD CONSTRAINT "PContent_fk_content_id" FOREIGN KEY ("content_id") REFERENCES "PContent" ("id");
 
+ALTER TABLE "UAlbom" 
+	ADD CONSTRAINT "User_fk_user_id" FOREIGN KEY ("user_id") REFERENCES "User" ("id");
+
+ALTER TABLE "UPhoto" 
+	ADD CONSTRAINT "UAlbom_fk_albom_id" FOREIGN KEY ("albom_id") REFERENCES "UAlbom" ("id");
+ALTER TABLE "UPhoto" 
+	ADD CONSTRAINT "Photo_fk_photo_id" FOREIGN KEY ("photo_id") REFERENCES "Photo" ("id");
+
 ALTER TABLE "UPosts" 
 	ADD CONSTRAINT "User_fk_user_id" FOREIGN KEY ("user_id") REFERENCES "User" ("id");
 ALTER TABLE "UPosts" 
@@ -218,3 +281,5 @@ ALTER TABLE "User"
 	ADD CONSTRAINT "City_fk_city_id" FOREIGN KEY ("city_id") REFERENCES "City" ("id");
 ALTER TABLE "User" 
 	ADD CONSTRAINT "Country_fk_country_id" FOREIGN KEY ("country_id") REFERENCES "Country" ("id");
+ALTER TABLE "User" 
+	ADD CONSTRAINT "Photo_fk_photo_id" FOREIGN KEY ("photo_id") REFERENCES "Photo" ("id");
--- sql/INSERT.sql
+++ sql/INSERT.sql
@@ -32,22 +32,47 @@ INSERT INTO "City"
 INSERT INTO "City"
 	VALUES (4, 'Самара', 1);
 
+INSERT INTO "Photo"
+	VALUES (1, 'public/style/img/face_petr.jpg');
+INSERT INTO "Photo"
+	VALUES (2, 'public/style/img/face_artem.jpg');
+INSERT INTO "Photo"
+	VALUES (3, 'public/style/img/face_sergey.jpg');
+INSERT INTO "Photo"
+	VALUES (4, 'public/style/img/face_kirill.jpg');
+INSERT INTO "Photo"
+	VALUES (5, 'public/style/img/comm_photo.jpg');
+INSERT INTO "Photo"
+	VALUES (6, 'public/style/img/dialog_photo.jpg');
+INSERT INTO "Photo"
+	VALUES (7, 'public/style/img/user_photo.jpg');
+INSERT INTO "Photo"
+	VALUES (8, 'public/style/img/user_2_photo.jpg');
+
 INSERT INTO "User" ("id", "email", "password", "phone", "first_name", "second_name", "middle_name",
-	"gender_id", "birthday", "country_id", "city_id", "photo", "verification", "private")
+	"gender_id", "birthday", "country_id", "city_id", "photo_id", "verification", "private")
 	VALUES (1, 'PetrovPP@gmail.com', 'passwrod123', '8-953-486-45-12', 'Петр', 'Петров', 'Петрович', 1,
-			'01.01.2000', 1, 3, 'public/style/img/face_petr.jpg', true, true);
+			'01.01.2000', 1, 3, 1, true, true);
 INSERT INTO "User" ("id", "email", "password", "phone", "first_name", "second_name", "middle_name",
-	"gender_id", "birthday", "country_id", "city_id", "photo", "verification", "private")
+	"gender_id", "birthday", "country_id", "city_id", "photo_id", "verification", "private")
 	VALUES (2, 'GrishkinAN@gmail.com', 'artemthegod', '8-957-123-45-98', 'Артем', 'Гришкин', 'Николаевич', 2,
-			'02.02.2000', 1, 4, 'public/style/img/face_artem.jpg', true, false);
+			'02.02.2000', 1, 4, 2, true, false);
 INSERT INTO "User" ("id", "email", "password", "phone", "first_name", "second_name", "middle_name",
-	"gender_id", "birthday", "country_id", "city_id", "photo", "verification", "private")
+	"gender_id", "birthday", "country_id", "city_id", "photo_id", "verification", "private")
 	VALUES (3, 'IgnatievSF@gmail.com', 'igsefe2000', '8-951-456-12-45', 'Сергей', 'Игнатьев', 'Федорович', 6,
-			'03.03.2000', 1, 1, 'public/style/img/face_sergey.jpg', false, true);
+			'03.03.2000', 1, 1, 3, false, true);
 INSERT INTO "User" ("id", "email", "password", "phone", "first_name", "second_name", "middle_name",
-	"gender_id", "birthday", "country_id", "city_id", "photo", "verification", "private")
+	"gender_id", "birthday", "country_id", "city_id", "photo_id", "verification", "private")
 	VALUES (4, 'PetrovKA@gmail.com', 'ruletheworld', '8-953-682-64-35', 'Кирилл', 'Петров', 'Анатольевич', 3,
-			'04.04.2000', 1, 2, 'public/style/img/face_kirill.jpg', false, false);
+			'04.04.2000', 1, 2, 4, false, false);
+
+INSERT INTO "UAlbom"
+	VALUES (1, 'Отдыхаю в Египте', 1);
+
+INSERT INTO "UPhoto"
+	VALUES (1, 7);
+INSERT INTO "UPhoto"
+	VALUES (1, 8);
 
 INSERT INTO "Friendlist"
 	VALUES (1, 2, 1);
@@ -94,6 +119,16 @@ INSERT INTO "Communities"
 INSERT INTO "Communities"
 	VALUES (2, 'НеНовости', 2, 'НеГруппа новостей', 'public/style/img/group.jpg');
 
+INSERT INTO "CAlbom"
+	VALUES (1, 'Новости в картинках', 1);
+INSERT INTO "CAlbom"
+	VALUES (2, 'НеНовости в картинках', 2);
+	
+INSERT INTO "CPhoto"
+	VALUES (1, 5);
+INSERT INTO "CPhoto"
+	VALUES (2, 5);
+
 INSERT INTO "Roles" 
 	VALUES (1, 'Владелец');
 INSERT INTO "Roles"
@@ -114,9 +149,14 @@ INSERT INTO "CPosts"
 	VALUES (2, 1);
 
 INSERT INTO "Dialog"
-	VALUES (1);
+	VALUES (1, 'Пётр, Кирилл');
 INSERT INTO "Dialog"
-	VALUES (2);
+	VALUES (2, 'Артём, Сергей');
+
+INSERT INTO "DPhoto"
+	VALUES (1, 6);
+INSERT INTO "DPhoto"
+	VALUES (2, 6);
 
 INSERT INTO "MContent"
 	VALUES (1, 'Текст сообщения');
@@ -162,12 +202,18 @@ DELETE FROM "Administrators";
 DELETE FROM "AllDialogs";
 DELETE FROM "UPosts";
 DELETE FROM "USubs";
+DELETE FROM "UPhoto";
+DELETE FROM "UAlbom";
 DELETE FROM "User";
+DELETE FROM "DPhoto";
 DELETE FROM "Dialog";
 DELETE FROM "MContent";
 DELETE FROM "Gender";
 DELETE FROM "Roles";
 DELETE FROM "CPosts";
+DELETE FROM "CPhoto";
+DELETE FROM "CAlbom";
+DELETE FROM "Photo";
 DELETE FROM "Communities";
 DELETE FROM "Topics";
 DELETE FROM "City";
